1ca436f383c9ed36dcac6cfbafe88fafe52a731e
johnmurray4 <jmurray2598@gmail.com>
2022-06-24 16:16:11 -0400

    styling changes

--- pages/AddPlantTrackingDetails.js
+++ pages/AddPlantTrackingDetails.js
@@ -1,20 +1,17 @@
 import React, { useState } from "react";
-import { useRouter } from "next/router";
 import styles from "../styles/tracking.module.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { Input, Button } from "@mui/material";
+import { Input, Button, Link } from "@mui/material";
 
 function AddPlantTrackingDetails() {
+  const todaysDate = new Date();
   const [species, setSpecies] = useState("");
-  const [dateObtained, setDateObtained] = useState(new Date());
+  const [dateObtained, setDateObtained] = useState(todaysDate);
   const [minDays, setMinDays] = useState(7);
   const [maxDays, setMaxDays] = useState(10);
-
-  const router = useRouter();
-  const back = () => {
-    router.back();
-  };
+  const [nextWater, setNextWater] = useState(todaysDate);
+  const [nextFeeding, setNextFeeding] = useState(todaysDate);
 
   const savePlantTrackingDetails = async (event) => {
     event.preventDefault();
@@ -25,6 +22,7 @@ function AddPlantTrackingDetails() {
       species: species,
       minDaysBetweenWatering: minDays,
       maxDaysBetweenWatering: maxDays,
+      
     };
     let response = await fetch(
       "http://localhost:8080/plant-tracking-details/save",
@@ -43,8 +41,8 @@ function AddPlantTrackingDetails() {
   const onChangeSpecies = (s) => {
     setSpecies(s);
   };
-  const onChangeDateObtained = (d) => {
-    setDateObtained(d);
+  const onChangeDateObtained = (date) => {
+    setDateObtained(date);
   };
   const incMinDays = (n) => {
     setMinDays(n);
@@ -52,27 +50,39 @@ function AddPlantTrackingDetails() {
   const incMaxDays = (n) => {
     setMaxDays(n);
   };
+  const onChangeNextWater = (date) => {
+    setNextWater(date);
+  };
+  const onChangeNextFeeding = (date) => {
+    setNextFeeding(date);
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.title}>
         <a>ADD PLANT TRACKING DETAILS</a>
       </div>
-      <button onClick={back}>X</button>
       <div className={styles.main}>
         <form className={styles.form}>
           <fieldset>
-            <label>species:</label>
-            <input className={styles.input} type="text" value={species} onChange={onChangeSpecies} />
-            <br></br>
-            <label htmlFor="dateObtained">had since: </label>
-            <DatePicker className={styles.input}
-              id="dateObtained"
-              selected={dateObtained}
-              onChange={onChangeDateObtained}
+            <label>
+              species:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </label>
+            <Input
+              className={styles.input}
+              type="text"
+              name="species"
+              id="minDays"
+              min="0"
+              value={species}
+              onChange={onChangeSpecies}
             />
-            <label htmlFor="minDays">min days between watering: </label>
-            <Input 
+            <br></br>
+            <label htmlFor="minDays">
+              min days between watering:
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </label>
+            <Input
               className={styles.input}
               type="number"
               defaultValue={7}
@@ -81,7 +91,10 @@ function AddPlantTrackingDetails() {
               min="0"
             />
             <br></br>
-            <label htmlFor="maxDays">max days between watering: </label>
+            <label htmlFor="maxDays">
+              max days between watering:
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </label>
             <Input
               className={styles.input}
               type="number"
@@ -90,9 +103,65 @@ function AddPlantTrackingDetails() {
               id="maxDays"
               min="0"
             />
+            <br></br>
+            <label htmlFor="nextWater">next watering date: </label>
+            <DatePicker
+              className={styles.input}
+              id="nextWater"
+              selected={nextWater}
+              onChange={onChangeNextWater}
+            />
+            <br></br>
+            <label htmlFor="nextFeeding">next feeding date: </label>
+            <DatePicker
+              className={styles.input}
+              id="nextFeeding"
+              selected={nextFeeding}
+              onChange={onChangeNextFeeding}
+            />
+            <br></br>
+            <label htmlFor="dateObtained">had since: </label>
+            <DatePicker
+              className={styles.input}
+              id="dateObtained"
+              selected={dateObtained}
+              onChange={onChangeDateObtained}
+            />
           </fieldset>
           <br></br>
-          <Button type="submit" onClick={savePlantTrackingDetails}>
+          <Link
+            href="/Tracking"
+            style={{
+              textDecoration: "none",
+              alignItems: "space-evenly",
+              alignContent: "space-evenly",
+              justifyContent: "space-evenly"
+            }}
+          >
+            <Button
+              variant="outlined"
+              style={{
+                color: "rgb(216, 216, 0)",
+                borderColor: "rgb(216, 216, 0)",
+              }}
+            >
+              Cancel
+            </Button>
+          </Link>
+          <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </span>
+          <Button
+            type="submit"
+            variant="contained"
+            style={{
+              backgroundColor: "rgb(216, 216, 0)",
+              color: "rgb(28, 61, 28)",
+            }}
+            onClick={savePlantTrackingDetails}
+          >
             Save
           </Button>
         </form>
