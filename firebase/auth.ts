48324c00c35644c5e40fb6ba6f157eaf2525706e
John Murray <jmurray2598@gmail.com>
2022-07-17 11:12:48 -0400

    Update auth.ts

--- firebase/auth.ts
+++ firebase/auth.ts
@@ -1,7 +1,9 @@
-import { getAuth } from 'firebase/auth';
+import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth';
 import app from './clientApp';
 
 // Initialize Auth object to be referenced within app
 const auth = getAuth(app);
+setPersistence(auth, browserLocalPersistence)
+    .then(() => console.log('using local storage persistence'));
 
 export default auth;
