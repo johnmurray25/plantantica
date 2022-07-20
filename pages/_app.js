e30bb8f4e5f17ab88ec43c17061d7b3299732e6a
John Murray <jmurray2598@gmail.com>
2022-07-20 14:09:08 -0400

    server side loading/progress indicators

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,13 @@
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+
 import '../styles/globals.css'
+
+// Bind loading/progress functions to Next event listeners
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
