574f72d6946ad4a85867a703187cf8a12a1a1220
John Murray <jmurray2598@gmail.com>
2022-07-21 11:39:10 -0400

    move getPlants, deletePlant into PlantService

--- pages/Tracking.tsx
+++ pages/Tracking.tsx
@@ -1,58 +1,20 @@
 import React, { Dispatch, SetStateAction, useCallback, useEffect, useState } from "react";
 import Link from "next/link";
 
-import { User } from "firebase/auth";
-import { collection, query, doc, getDocs, deleteDoc } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { IoRefresh } from '@react-icons/all-files/io5/IoRefresh';
+// import { IoRefresh } from '@react-icons/all-files/io5/IoRefresh';
 import ReactLoading from 'react-loading';
 
 import auth from '../firebase/auth';
-import db from '../firebase/db';
 import NavBar from "./components/NavBar";
 import PlantTrackingDetails from "./components/PlantTrackingDetails";
 import styles from "../styles/tracking.module.css";
 import Plant from "../domain/Plant";
+import { getPlants, deletePlant } from "../service/PlantService";
 
 const OK = 200;
 const UNAUTHORIZED = 403;
 const ERR_STATUS = 500;
-
-const getPlants = async (user: User) => {
-  try {
-    if (!user) return;
-    // Load all plant tracking data for current user
-    const collectionRef = collection(doc(db, 'users', user.email), 'plantTrackingDetails');
-    const queryRef = query(collectionRef);
-    const trackingDetails = await getDocs(queryRef);
-    return trackingDetails.docs
-      .map((doc): Plant => {
-        return {
-          id: doc.id,
-          species: doc.get('species'),
-          dateObtained: new Date(doc.get('dateObtained')),
-          daysBetweenWatering: doc.get('daysBetweenWatering'),
-          dateLastWatered: new Date(doc.get('dateLastWatered')),
-          dateToWaterNext: new Date(doc.get('dateToWaterNext')),
-          dateLastFed: new Date(doc.get('dateLastFed')),
-          dateToFeedNext: new Date(doc.get('dateToFeedNext')),
-          lightRequired: doc.get('lightRequired'),
-          dateCreated: new Date(doc.get('dateCreated')),
-          picture: doc.get('picture'),
-        }
-      });
-  } catch (error) {
-    console.error(error);
-    console.error("Failed to fetch plant tracking details");
-    throw error;
-  }
-};
-
-const deletePlant = async (plant: Plant, user: User) => {
-  confirm(`Delete ${plant.species}?`);
-  await deleteDoc(doc(collection(doc(db, 'users', user.email), 'plantTrackingDetails'), plant.id));
-  console.log('deleted plant');
-}
 
 const Home = () => {
   const [plants, setPlants]: [Plant[], Dispatch<SetStateAction<Plant[]>>] = useState([]);
--- service/PlantService.ts
+++ service/PlantService.ts
@@ -1,6 +1,9 @@
 import { User } from "firebase/auth";
+import { collection, deleteDoc, doc, getDocs, query } from "firebase/firestore";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import Plant from "../domain/Plant";
 
+import db from '../firebase/db';
 import storage from '../firebase/storage';
 
 export const getImageUrl = async (fileName: string, user: User): Promise<string> => {
@@ -20,4 +23,40 @@ export const uploadFile = async (file: File, user: User) => {
     let fileRef = await uploadBytes(storageRef, bytes);
     console.log(`uploaded image: ${fileRef.ref.fullPath}`)
     return fileRef.ref.name;
+}
+
+export const getPlants = async (user: User) => {
+    try {
+        if (!user) return;
+        // Load all plant tracking data for current user
+        const collectionRef = collection(doc(db, 'users', user.email), 'plantTrackingDetails');
+        const queryRef = query(collectionRef);
+        const trackingDetails = await getDocs(queryRef);
+        return trackingDetails.docs
+            .map((doc): Plant => {
+                return {
+                    id: doc.id,
+                    species: doc.get('species'),
+                    dateObtained: new Date(doc.get('dateObtained')),
+                    daysBetweenWatering: doc.get('daysBetweenWatering'),
+                    dateLastWatered: new Date(doc.get('dateLastWatered')),
+                    dateToWaterNext: new Date(doc.get('dateToWaterNext')),
+                    dateLastFed: new Date(doc.get('dateLastFed')),
+                    dateToFeedNext: new Date(doc.get('dateToFeedNext')),
+                    lightRequired: doc.get('lightRequired'),
+                    dateCreated: new Date(doc.get('dateCreated')),
+                    picture: doc.get('picture'),
+                }
+            });
+    } catch (error) {
+        console.error(error);
+        console.error("Failed to fetch plant tracking details");
+        throw error;
+    }
+};
+
+export const deletePlant = async (plant: Plant, user: User) => {
+    confirm(`Delete ${plant.species}?`);
+    await deleteDoc(doc(collection(doc(db, 'users', user.email), 'plantTrackingDetails'), plant.id));
+    console.log('deleted plant');
 }
