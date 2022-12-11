43e9c41bf848de2eefcd81a3f055e9307ca39692
johnmurray25 <47039859+johnmurray25@users.noreply.github.com>
2022-12-11 14:19:12 -0500

    cont.

--- next.config.js
+++ next.config.js
@@ -2,13 +2,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: [
       'firebasestorage.googleapis.com',
-      'plantantica.appspot.com',
+      'plantantica.appspot.com', 
       'www.gstatic.com',
+      'plantantica.com',
+      'plantantica.web.app',
+      'plantantica.firebase.app',
+      'plantantica.vercel.app',
     ],
-    loader: "custom"
+    // loader: "custom",
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'firebasestorage.googleapis.com/v0/b',
+    //     pathname: '/plantantica.appspot.com/**',
+    //   },
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'plantantica.com',
+    //     pathname: '/_next/image/**',
+    //   }
+    // ],
   },
   // target: "serverless",
   // assetPrefix: isProd ? "https://us-central1-plantantica.cloudfunctions.net/handler" : "",
